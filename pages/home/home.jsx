39e7b5bc5d95b7ec952d1b2801519e6ecccbb413
ORIENTSOFTWARE\hoan.tran <hoan.tran@orientsoftware.com>
2022-01-21 10:50:46 +0700

    feature: add comment for unused component

--- pages/home/home.jsx
+++ pages/home/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import NavigationHeader from "../../components/navigation-header/navigation-header";
-import NavigationFooter from "../../components/navigation-footer/navigation-footer";
+// import NavigationHeader from "../../components/navigation-header/navigation-header";
+// import NavigationFooter from "../../components/navigation-footer/navigation-footer";
 export default function Home() {
   return (
     <p>ahahahahahaha</p>
